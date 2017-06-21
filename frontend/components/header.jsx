69368c4e722d4cd3057dddb81228d763f64bb13d
Dexter Sealy <dextersealy@gmail.com>
2017-06-21 11:41:25 -0400

    Drop Greeting component

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
+import { logout } from '../actions/session_actions';
+import { connect } from 'react-redux';
 import Logo from './logo';
-import Greeting from './greeting';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout() {
+    this.props.logout();
+  }
 
   render() {
+    const { user } = this.props;
     return (
       <header>
         <Logo/>
-        <Greeting/>
+        { this.props.user &&
+          <div className='greeting'>
+            <p className='name'>{`${user.name}`}</p>
+            <button onClick={this.handleLogout}>Sign Out</button>
+          </div>
+        }
       </header>
     );
   }
-
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  user: state.session.currentUser
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(logout())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
