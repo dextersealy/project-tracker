1097eed8305783cfb317637d4561305f888b59ad
Dexter Sealy <dextersealy@gmail.com>
2017-06-24 10:37:41 -0400

    Refactor StoryIndex

--- frontend/components/story/story_index.jsx
+++ frontend/components/story/story_index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import StoryIndexItem from './story_index_item';
 
-const StoryIndex = ({title, stories}) => {
-  const items = stories
-    ? stories.map(story => <StoryIndexItem key={story.id} story={story} />)
-    : null;
-  return (
-    <div className='index'>
-      <ul>{items}</ul>
-    </div>
-  );
-};
+class StoryIndex extends React.Component {
+  render() {
+    return (
+      <div className='index'>
+        <ul>{this.renderItems(this.props.stories)}</ul>
+      </div>
+    );
+  }
+
+  renderItems(stories) {
+    const items = stories && stories.map(story =>
+      <StoryIndexItem
+        key={story.id}
+        story={story}
+        />
+    );
+    return items;
+  }
+}
 
 export default StoryIndex;
