34f3a98d902411779222725674c911dde0c0b257
Dexter Sealy <dextersealy@gmail.com>
2017-06-29 00:10:25 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 user = User.find_or_create_by(email: 'dsealy@alum.mit.edu') do |user|
   user.name =  "Guest User"
   user.initials = 'GU',
@@ -24,26 +23,114 @@ if !project
 end
 
 stories = [
-  { state: :finished, title: "New user can sign up",
+  { state: :accepted,
+    title: "Phase 1: New user can sign up",
     description: <<~HEREDOC },
     Sign Up button is on home page. Sign Up page enforces password guidelines
     and prevents duplicate username or email.
     HEREDOC
-  { state: :started, title: "User can sign in",
+  { state: :accepted,
+    title: "Phase 1: User can sign in",
     description: <<~HEREDOC },
     Sign in button is on the home page. Sign Up page validates account
     information.
     HEREDOC
-  { state: :unstarted, title: "User remains signed in",
+  { state: :accepted,
+    title: "Phase 1: User remains signed in",
     description: <<~HEREDOC },
     The user remains signed in when the page is refreshed.
     HEREDOC
+
+  { state: :accepted,
+    title: "Phase 2: User can create/rename projects",
+    tasks: [
+      {title: "Style the project dashboard", done: true},
+      {title: "After create, open new project immediately", done: true}
+    ],
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :delivered,
+    title: "Phase 3: User can create stories",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :delivered,
+    title: "Phase 3: User can edit stories",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :rejected,
+    title: "Phase 3: User can delete stories",
+    tasks: [{title: "Only owner can delete story"}],
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :started, kind: :release,
+    title: "Demo day!",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :finished,
+    title: "Phase 4: User can move stories through workflow",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :started,
+    title: "Phase 4: Only story owner can Accept or Reject",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :unstarted,
+    title: "Phase 4: User can toggle hiding/showing accepted stories",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :accepted,
+    title: "Phase 5: User can create and edit tasks",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :accepted,
+    title: "Phase 5: User can change task status",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :unstarted, kind: :bug,
+    title: "Editing a task discards pending edits to other story attributes",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :unstarted,
+    title: "Phase 6: User can comment on story",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :unstarted,
+    title: "Phase 6: User can edit own comments",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :unstarted,
+    title: "Phase 6: User who rejects story is prompted to enter a comment",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :delivered,
+    title: "Phase 7: User can prioritize stories via drag & drop",
+    description: <<~HEREDOC },
+    HEREDOC
+  { state: :started, kind: :chore,
+    title: "Use callbacks to pass state changes up the component hierarchy.",
+    description: <<~HEREDOC },
+    HEREDOC
+
 ]
 
-stories.each do |story|
-  object = Story.find_or_create_by({ project: project, title: story[:title] })
-  object.update!({
-    author: user, owner: user, state: story[:state],
-    description: story[:description].gsub(/\s+/, " ").strip
-  })
+Story.transaction do
+  priority = Story.maximum(:priority)
+  stories.each do |story|
+    priority += 1
+    storyObj = Story.find_or_create_by({ project: project, title: story[:title] })
+    storyObj.update!({
+      author: user, owner: user, state: story[:state], priority: priority,
+      description: story[:description].gsub(/\s+/, " ").strip,
+      kind: story[:kind] || :feature
+    })
+
+    if story[:tasks]
+      story[:tasks].each do |task|
+        taskObj = Task.find_or_create_by({
+          story: storyObj, author: storyObj.author, title: task[:title]
+          })
+        taskObj.update!(done: true) if task[:done]
+      end
+    end
+  end
 end
