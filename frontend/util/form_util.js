9147c93081f12eab8b0a03369429f87c89773be1
Dexter Sealy <dextersealy@gmail.com>
2017-06-30 13:53:25 -0400

    Simplify FormUtil.handleChange

--- frontend/util/form_util.js
+++ frontend/util/form_util.js
@@ -1,8 +1,6 @@
 export const handleChange = function(field) {
   return (e) => {
     e.preventDefault();
-    const newState = Object.assign({}, this.state);
-    newState[field] = e.currentTarget.value;
-    this.setState(newState);
+    this.setState({[field]: e.currentTarget.value});
   }
 };
