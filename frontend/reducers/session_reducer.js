78c6ce9ed3961d59d5b5eb4d3ced9876d2286d28
Dexter Sealy <dextersealy@gmail.com>
2017-06-30 09:06:17 -0400

    Bugfix: don't overwrite pusher instance in session reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -5,7 +5,7 @@ const defaultState = { currentUser: null, pusher: null };
 const SessionReducer = (state = defaultState, action) => {
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      return { currentUser: action.user }
+      return Object.assign({}, state, { currentUser: action.user });
     default:
       return state;
   }
