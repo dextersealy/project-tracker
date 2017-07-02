5a43a010035b63dc3042ed9690bd41cb5e24fa7e
Dexter Sealy <dextersealy@gmail.com>
2017-07-02 10:29:24 -0400

    Refactor ProjectIndexItem

--- frontend/components/dashboard/project_index_item.jsx
+++ frontend/components/dashboard/project_index_item.jsx
@@ -6,27 +6,41 @@ import { deleteProject } from '../../actions/project_actions';
 class ProjectIndexItem extends React.Component {
   constructor(props) {
     super(props);
+
     this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  render() {
+    return (
+      <li className='item'>
+        {this.renderTitle()}
+        {this.renderActions()}
+      </li>
+    );
+  }
+
+  renderTitle() {
+    const { project } = this.props;
+    return (
+      <Link className='title' to={`/project/${project.id}`}>
+        {project.title}
+      </Link>
+    );
+  }
+
+  renderActions() {
+    const { project } = this.props;
+    return (
+      <div className='controls'>
+        <Link className='button' to={`/projects/edit/${project.id}`}>Edit</Link>
+        <button type='button' onClick={this.handleDelete}>Delete</button>
+      </div>
+    );
   }
 
   handleDelete(e) {
     e.preventDefault();
     this.props.deleteProject(this.props.project);
-  }
-
-  render() {
-    const { project } = this.props;
-    return (
-      <li className='item'>
-        <Link className='title' to={`/project/${project.id}`}>
-          {project.title}
-        </Link>
-        <div className='controls'>
-          <Link className='button' to={`/projects/edit/${project.id}`}>Edit</Link>
-          <button type='button' onClick={this.handleDelete}>Delete</button>
-        </div>
-      </li>
-    );
   }
 }
 
