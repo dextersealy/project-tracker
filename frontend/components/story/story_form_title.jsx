8fbf4f7ad99fb8f3e48254c126a1c7595a02c3c8
Dexter Sealy <dextersealy@gmail.com>
2017-07-03 17:42:51 -0400

    Address minification warnings; omit development tools from production

--- frontend/components/story/story_form_title.jsx
+++ frontend/components/story/story_form_title.jsx
@@ -4,7 +4,12 @@ const StoryTitle = ({ story, handleCaret, handleChange })  => {
   return (
     <section className='story-title-section'>
       <i onClick={handleCaret} className='caret fa fa-caret-down'/>
-      <input type='text' value={story.title} onChange={handleChange}/>
+      <input
+        type='text'
+        value={story.title}
+        onChange={handleChange}
+        autoFocus={true}
+        placeholder={'Enter a title'}/>
     </section>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,24 @@
 const path = require('path');
+const webpack = require("webpack");
+
+const plugins = []; // if using any plugins for both dev and production
+const devPlugins = []; // if using any plugins for development
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
 
 module.exports = {
   context: __dirname,
@@ -22,5 +42,6 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-maps'
+  devtool: 'source-maps',
+  plugins,
 };
