207531a2ee8310e3144bb40289439f7a3bbfcf5b
Dexter Sealy <dextersealy@gmail.com>
2017-07-19 17:50:08 -0400

    Add contact links

---
+++ frontend/components/util/contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <a href="https://github.com/dextersealy/project-tracker" target="_blank"
+         alt="GitHub Repository"><i className="fa fa-github"></i></a>
+      <a href="https://www.linkedin.com/in/dextersealy" target="_blank"
+         alt="LinkedIn Profile"><i className="fa fa-linkedin"></i></a>
+      <a href="mailto:dsealy@alum.mit.edu" alt="Send Mail">
+        <i className="fa fa-envelope-o"></i></a>
+    </div>
+  );
+}
+
+export default Contact;
--- frontend/components/util/header.jsx
+++ frontend/components/util/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Logo from './logo';
 import Greeting from './greeting';
+import Contact from './contact';
 
 const Header = () => {
   return (
     <header>
       <Logo/>
+      <Contact/>
       <Greeting/>
     </header>
   );
